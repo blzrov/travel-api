bbd873d12696f15282ea273bb476ce0080c83cc7
Rem <suhomesovo@gmail.com>
2023-01-26 00:17:51 +0500

    adsad

---
+++ youAreOrganize.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+export default async function youAreOranize(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+  await client.connect();
+  const collectionUsers = client.db("Project2022UrFu").collection("Users");
+  const collectionTravels = client.db("Project2022UrFu").collection("Travels");
+  const user = await collectionUsers.findOne({ login: req.params.login });
+  const travels = await collectionTravels
+    .find({ organizer: { $in: user.name } })
+    .toArray();
+  travels.forEach((element) => {
+    element.isFavorite = true;
+  });
+  const travelJson = JSON.stringify(travels);
+  res.send(travelJson || false);
+  console.log(travelJson);
+  res.send(travelJson || false);
+  if (user == null) {
+    return false;
+  } else {
+    return travelJson;
+  }
+}
